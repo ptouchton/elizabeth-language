582821815274dc8d17853ebde8149c77b56ee8be
Paul Touchton <paultouchton@gmail.com>
2021-10-10 09:22:39 -0700

    initial build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, from, of, Subject } from 'rxjs'
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AppComponent {
   title = 'elizabeth-language';
+
+  private plainInputSubject = new BehaviorSubject<string[]>(['Welcome']);
+  plainInput$ = this.plainInputSubject.asObservable();
+  
+  translatedOutput$ = this.plainInput$
+  .pipe(
+    map((val) => val.map((i: any) => `e${i}th`)),
+    map((val) => val.join(' ')),
+    tap(val => console.log(val))
+  );
+
+
+  vm$ = combineLatest([
+    this.plainInput$,
+    this.translatedOutput$,
+  ])
+    .pipe(
+      map(([plainInput, translatedOutput]) =>
+        ({ plainInput, translatedOutput }))
+    );
+
+    onChange(input: any) {
+      const arr = input.split(' ');
+      this.plainInputSubject.next(arr);
+    }  
+
+    onClear(){
+      this.plainInputSubject.next([]);
+    }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TextFieldModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
